c207dcc10d47e33726dcfa53dcedf8e29687077a
Frederik Gelder <frederik.gelder@freenet.de>
2024-05-20 16:41:42 +0200

    forgot a bit

--- certbot/request_cert.sh
+++ certbot/request_cert.sh
@@ -3,4 +3,4 @@ set -o nounset
 domain=$1
 mail=$2
 
-certbot certonly --webroot -w /var/www/html -d "$1" --non-interactive --agree-tos -m
+certbot certonly --webroot -w /var/www/html -d "$1" --non-interactive --agree-tos -m "$2"
