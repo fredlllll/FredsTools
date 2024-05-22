688e82be3bea177b7c411028a4fc01dc3be673cc
Frederik Gelder <frederik.gelder@freenet.de>
2024-05-22 14:20:55 +0200

    fixes

--- docker/templates/project/build.sh
+++ docker/templates/project/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+CONFIGURATION="${CONFIGURATION:-prod}"
+
 pushd "$(dirname "${BASH_SOURCE[0]}")" || exit
-source init/init.sh
+. init/init.sh
 
 target_image=%%project_name%%:latest
 dockerfile=%%project_name%%.dockerfile
 
-docker build "$@" -f $dockerfile --tag $target_image .
+docker build "$@" -f $dockerfile --tag $target_image . || exit
 
 popd +0 || exit
--- docker/templates/project/init/init.sh
+++ docker/templates/project/init/init.sh
@@ -4,14 +4,14 @@ if [[ -z "$INIT_RAN" ]]; then
   echo "running init"
   scriptsdir="$(dirname "${BASH_SOURCE[0]}")/scripts"
   pushd "$scriptsdir" || exit
-  for file in "$scriptsdir"/*.sh; do
+  for file in *.sh; do
     if [ -f "$file" ]; then
       source "$file"
     fi
   done
   echo "finished init"
   popd +0 || exit
-  export INIT_RAN=true
+  INIT_RAN=true
 else
   echo "init has already ran, skipping"
 fi
