9cb73bfe1dc09d20af8e6ee1f0eb3810374da8eb
Frederik Gelder <frederik.gelder@freenet.de>
2024-11-08 23:57:05 +0100

    add update repo function

--- docker/templates/project/init/init.sh
+++ docker/templates/project/init/init.sh
@@ -4,6 +4,24 @@ if [[ -z "$INIT_RAN" ]]; then
   echo "running init"
 
   #functions
+  clone_update_repo() {
+    local repo_name=$1
+    local repo_url=$2
+    local branch_or_commit="$(cat "$CONFIGDIR"/branches/"$repo_name")"
+    pushd "$CONFIGDIR"/repos
+    if [ ! -d ./"$repo_name" ]; then
+      (
+        git clone --no-checkout --recurse-submodules "$repo_url" "$repo_name"
+      )
+    fi
+    pushd "$repo_name"
+    git fetch
+    git checkout "$branch_or_commit"
+    git pull origin "$branch_or_commit"
+    git submodule update --init --recursive
+    popd
+    popd
+  }
   #functions end
 
   scriptsdir="$(dirname "${BASH_SOURCE[0]}")/scripts"
